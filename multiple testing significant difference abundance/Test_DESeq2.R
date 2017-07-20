0a019462a0c5f0acdce67fa1eb11e4acf1595c7f
psBiostat <perrine.soret@gmail.com>
2017-07-20 09:48:11 +0200

    Update Test_DESeq2.R

--- multiple testing significant difference abundance/Test_DESeq2.R
+++ multiple testing significant difference abundance/Test_DESeq2.R
@@ -1,1 +1,33 @@
 
+data("Koren.16S")
+
+koren.Count= Koren.16S$data.raw
+Grp  =  Koren.16S$indiv$body_site
+
+#We choose two boysites
+koren.Count = koren.Count[-which(Grp == "UBERON:artery wall"), ]
+Grp <- factor(Grp[-which(Grp == "UBERON:artery wall")],
+levels = c("UBERON:feces", "UBERON:mouth"))
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("DESeq2")
+library("DESeq2")
+
+
+colData <- data.frame(condition = Grp)
+rownames(colData) <- row.names(koren.Count)
+
+#Data creaion
+dds <- DESeqDataSetFromMatrix(countData = t(koren.Count),
+                              colData = DataFrame(Grp),
+                              design = ~ Grp)
+
+#Pre-filtering
+dds <- dds[ rowSums(counts(dds)) > 1, ]
+
+#Test
+dds <- DESeq(dds)
+res <- results(dds)
+
+#Graphics
+plotMA(res, main="DESeq2", ylim=c(-2,2))
